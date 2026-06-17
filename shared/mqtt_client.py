398e09fa9b7308c51b2a582b92e1929a54090433
Daniel Schutterop <daniel@schutterop.nl>
2026-06-17 15:22:11 +0200

    Fix MQTT status subscriptions

--- shared/mqtt_client.py
+++ shared/mqtt_client.py
@@ -7,6 +7,7 @@ import os
 import time
 from collections.abc import Callable
 from dataclasses import dataclass
+from threading import Lock
 
 import paho.mqtt.client as mqtt
 
@@ -28,12 +29,28 @@ class MinyadMqttClient:
         self.client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, client_id=client_id)
         self.client.on_connect = self._on_connect
         self.client.on_disconnect = self._on_disconnect
+        self.client.on_message = self._on_message
+        self.client.reconnect_delay_set(min_delay=1, max_delay=60)
+        self._subscriptions: dict[str, Callable[[str, bytes], None]] = {}
+        self._subscriptions_lock = Lock()
 
-    def _on_connect(self, _client: mqtt.Client, _userdata: object, _flags: mqtt.ConnectFlags, reason_code: mqtt.ReasonCode, _properties: mqtt.Properties | None) -> None:
+    def _on_connect(self, client: mqtt.Client, _userdata: object, _flags: mqtt.ConnectFlags, reason_code: mqtt.ReasonCode, _properties: mqtt.Properties | None) -> None:
         LOGGER.info("MQTT connected: %s", reason_code)
+        with self._subscriptions_lock:
+            subscriptions = list(self._subscriptions)
+        for topic in subscriptions:
+            client.subscribe(topic)
+            LOGGER.info("MQTT subscribed: %s", topic)
 
     def _on_disconnect(self, _client: mqtt.Client, _userdata: object, _flags: mqtt.DisconnectFlags, reason_code: mqtt.ReasonCode, _properties: mqtt.Properties | None) -> None:
         LOGGER.warning("MQTT disconnected: %s", reason_code)
+
+    def _on_message(self, _client: mqtt.Client, _userdata: object, message: mqtt.MQTTMessage) -> None:
+        with self._subscriptions_lock:
+            subscriptions = list(self._subscriptions.items())
+        for topic_filter, handler in subscriptions:
+            if mqtt.topic_matches_sub(topic_filter, message.topic):
+                handler(message.topic, message.payload)
 
     def connect_forever(self) -> None:
         while True:
@@ -52,8 +69,6 @@ class MinyadMqttClient:
         self.client.publish(f"minyad/{source}/{measurement}", payload=str(payload), qos=0, retain=False)
 
     def subscribe(self, topic: str, handler: Callable[[str, bytes], None]) -> None:
-        def on_message(_client: mqtt.Client, _userdata: object, message: mqtt.MQTTMessage) -> None:
-            handler(message.topic, message.payload)
-
-        self.client.on_message = on_message
+        with self._subscriptions_lock:
+            self._subscriptions[topic] = handler
         self.client.subscribe(topic)
